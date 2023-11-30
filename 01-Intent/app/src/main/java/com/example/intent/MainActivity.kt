201ce62f142230341e5d243c09edddd3d5c8d948
DRucci-88 <d.rucci.2001@gmail.com>
2023-12-01 03:13:07 +0700

    01 Intent With Data

--- 01-Intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ 01-Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -9,13 +9,16 @@ import android.widget.Button
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnMoveActivity: Button
+    private lateinit var btnMoveActivityWithData: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivityWithData = findViewById(R.id.btn_move_activity_data)
 
         btnMoveActivity.setOnClickListener(this)
+        btnMoveActivityWithData.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
@@ -23,6 +26,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_move_activity -> {
                 val moveToCalculator = Intent(this@MainActivity, CalculatorActivity::class.java)
                 startActivity(moveToCalculator)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveToCalculatorWithData =
+                    Intent(this@MainActivity, CalculatorActivity::class.java)
+                moveToCalculatorWithData.putExtra(CalculatorActivity.EDIT_WIDTH, 50.9)
+                moveToCalculatorWithData.putExtra(CalculatorActivity.EDIT_HEIGHT, 11.7)
+                moveToCalculatorWithData.putExtra(CalculatorActivity.EDIT_LENGTH, 23.1)
+                startActivity(moveToCalculatorWithData)
             }
         }
     }
